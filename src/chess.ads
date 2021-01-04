40097961d44ab5cf740a49f2698fa9e4305d1586
Arthus Leroy <arthus.leroy@epita.fr>
2021-01-04 20:29:38 +0100

    [CMD] chess: implement is_valid_move

--- src/chess.ads
+++ src/chess.ads
@@ -1,19 +1,23 @@
 with Ada.Text_IO; use Ada.Text_IO;
 package Chess is
-  size : constant := 12;
-  subtype game_size is Integer range 1 .. size;
-  type Cell is (empty, forbidden,
-                pawn_white, rook_white, knight_white, bishop_white, queen_white, king_white,
-                pawn_black, rook_black, knight_black, bishop_black, queen_black, king_black);
+	size : constant := 12;
+	subtype game_size is Integer range 1 .. size;
+	type Cell is (empty, forbidden,
+                  pawn_white, rook_white, knight_white, bishop_white, queen_white, king_white,
+                  pawn_black, rook_black, knight_black, bishop_black, queen_black, king_black);
+	subtype white_range is Natural range 3 .. 8;
+	subtype black_range is Natural range 9 .. 14;
 
-   type Board is array (game_size,game_size) of Cell;
+	type Player is (White, Black);
 
-   game_board : Board;
+	type Board is array (game_size,game_size) of Cell;
 
-  function is_valid_move(x_start : in Integer; y_start : in Integer; x_end : in Integer; y_end : in Integer) return Boolean;
-  procedure init_gameboard;
-  procedure move_piece(x_start : in Integer; y_start : in Integer; x_end : in Integer; y_end : in Integer);
-  procedure print_gameboard;
+	game_board : Board;
+
+	function is_valid_move(x_start : in Integer; y_start : in Integer; x_end : in Integer; y_end : in Integer; p : in Player) return Boolean;
+	procedure init_gameboard;
+	procedure move_piece(x_start : in Integer; y_start : in Integer; x_end : in Integer; y_end : in Integer);
+	procedure print_gameboard;
 private
 
 end Chess;
--- src/main.adb
+++ src/main.adb
@@ -2,31 +2,39 @@ with Ada.Text_IO; use Ada.Text_IO;
 with Chess; use Chess;
 
 procedure main is
-  Str  : String (1 .. 80);
-  Last : Natural;
-  x_start : Integer;
-  y_start : Integer;
-  x_end : Integer;
-  y_end : Integer;
+	Str  : String (1 .. 80);
+	Last : Natural;
+
+	x_start : Integer;
+	y_start : Integer;
+	x_end : Integer;
+	y_end : Integer;
+
+	side 	: Player := White;
 begin
-  init_gameboard;
-  print_gameboard;
-  Put_Line("Welcome to Adachess");
-  Put_Line("white to move:");
+	init_gameboard;
+	print_gameboard;
+	Put_Line("Welcome to Adachess");
 
-  Main_Loop:
-  while(True) loop -- is_game_end()
-    Ada.Text_IO.Get_Line (Str, Last);
-    x_start := Character'Pos(Str(1)) - Character'Pos('A') + 3;
-    y_start := Integer'Value(Str(2 .. 2)) + 2;
-    x_end := Character'Pos(Str(4)) - Character'Pos('A') + 3;
-    y_end := Integer'Value(Str(5 .. 5)) + 2;
+	while(True) loop -- is_game_end()
+		Put_Line((if side = White then "White" else "Black") & " to move:");
 
-    if not is_valid_move(x_start , y_start, x_end, y_end) then
-      Put_Line("Input a valid move");
-    else
-      move_piece(x_start, y_start, x_end, y_end);
-      print_gameboard;
-    end if;
-  end loop Main_Loop;
+		-- TODO: take care of castling
+
+		Ada.Text_IO.Get_Line (Str, Last);
+    	x_start := Character'Pos(Str(1)) - Character'Pos('A') + 3;
+    	y_start := Integer'Value(Str(2 .. 2)) + 2;
+    	x_end := Character'Pos(Str(4)) - Character'Pos('A') + 3;
+    	y_end := Integer'Value(Str(5 .. 5)) + 2;
+
+    	if not is_valid_move(x_start, y_start, x_end, y_end, side) then
+      		Put_Line("Input a valid move");
+	    else
+			-- TODO: take care of cases where king is check
+			-- TODO: transform a pawn into queen if pawn at last row
+
+		    move_piece(x_start, y_start, x_end, y_end);
+      		print_gameboard;
+    	end if;
+  	end loop;
 end main;
