ab455c11d7362c99a988019661fa36ef8b852836
Arthus Leroy <arthus.leroy@epita.fr>
2021-01-05 17:26:47 +0100

    [CMD] main.adb: remove offset at it's handled somewhere else

--- src/main.adb
+++ src/main.adb
@@ -1,5 +1,6 @@
 with Ada.Text_IO; use Ada.Text_IO;
 with Chess; use Chess;
+with Move; use Move;
 
 procedure main is
     Str     : String (1 .. 80);
@@ -25,10 +26,10 @@ begin
         -- TODO: take care of castling
 
         Ada.Text_IO.Get_Line (Str, Last);
-        x_start := Character'Pos(Str(1)) - Character'Pos('A') + 3;
-        y_start := Integer'Value(Str(2 .. 2)) + 2;
-        x_end := Character'Pos(Str(4)) - Character'Pos('A') + 3;
-        y_end := Integer'Value(Str(5 .. 5)) + 2;
+        x_start := Character'Pos(Str(1)) - Character'Pos('A') + 1;
+        y_start := Integer'Value(Str(2 .. 2));
+        x_end := Character'Pos(Str(4)) - Character'Pos('A') + 1;
+        y_end := Integer'Value(Str(5 .. 5));
 
         if not is_valid_move(x_start, y_start, x_end, y_end, side, prev) then
             Put_Line("Input a valid move");
