70dbc66807afffd8509980f175462d64a49f0c09
Arthus Leroy <arthus.leroy@epita.fr>
2021-01-22 23:59:34 +0100

    [CMD] main.adb: clean and update main

--- src/main.adb
+++ src/main.adb
@@ -10,44 +10,53 @@ procedure main is
 
     function check_input return Boolean is
     begin
-        Ada.Text_IO.Get_Line (Str, Last);
+        Get_Line(Str, Last);
+
+        -- TODO: take care of castling
 
         if Str(1 .. Last)'Length /= 4 then
             return False;
         end if;
 
-	    Move.Start.X := Character'Pos(Str(1)) - 96;
-        Move.Start.Y := Character'Pos(Str(2)) - 48;
-        Move.Target.X := Character'Pos(Str(3)) - 96;
-        Move.Target.Y := Character'Pos(Str(4)) - 48;
+	    Move.Start.X := Character'Pos(Str(Str'First + 0)) - 96;
+        Move.Start.Y := Character'Pos(Str(Str'First + 1)) - 48;
+        Move.Target.X := Character'Pos(Str(Str'First + 2)) - 96;
+        Move.Target.Y := Character'Pos(Str(Str'First + 3)) - 48;
 
         return True;
     exception
         when others => return False;
     end check_input;
+
+	function get_player_name(Player : in Player_Type) return String is
+	begin
+		return (if Player = White then "White" else "Black");
+	end get_player_name;
 begin
     Read_Fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 56);
     Put_Line("Welcome to Adachess");
 
     while(True) loop -- is_game_end()
-        Put_Line((if Player = White then "White" else "Black") & " to move:");
+		Put_Line("");
+        Put_Line(get_player_name(Player) & " to move:");
 	    Print;
-
-        -- TODO: take care of castling
+		Put_Line("");
 
         if not check_input then
             if Str(1 .. 4) = "quit" then
-                Put_Line((if Player = White then "Black" else "White") & " win ! exiting game.");
+				Put_Line("");
+                Put_Line(get_player_name(Player) & " win ! exiting game.");
                 exit;
             end if;
-            Put_Line("Wrong input, Usage: a2a4.");
+            Put_Line("Invalid input. Usage: a2a4.");
         elsif not is_valid_move(Move, Player) then
-            Put_Line("Input a valid move.");
+            Put_Line("Invalid move : " & Str(1 .. Last));
         else
-            -- TODO: take care of cases where king is check
-            -- TODO: transform a pawn into queen if pawn at last row
             move_piece(Move);
-			End_Turn;
+			if End_Turn then
+				Put_Line(get_player_name(Player) & "'s King checkmate.");
+				exit;
+			end if;
         end if;
     end loop;
 end main;
