8cde3ae5c048d25138575a557c77ddc22e7ef6b6
Arthus Leroy <arthus.leroy@epita.fr>
2021-02-08 14:50:57 +0100

    [CMD] test.adb: add debug option

--- src/test.adb
+++ src/test.adb
@@ -1,8 +1,17 @@
+with Ada.Command_Line; use Ada.Command_Line;
+
+with chess; use chess;
 with chess_tb; use chess_tb;
 with move_tb; use move_tb;
 
 procedure test is
 begin
+	for I in 1 .. Argument_Count loop
+		if Argument(I) = "--debug" or Argument(I) = "-d" then
+			Set_Debug(True);
+		end if;
+	end loop;
+
 	test_chess_tb;
 	test_move_tb;
 end test;
